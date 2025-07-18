9882db2e2272ea7aa8b2f84cd084d79fd377abaf
Jesus Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2025-07-18 13:28:36 -0400

    EVChargingStationsAPIService implementation WIP

--- EV Charging Station Finder/Data/DataSources/EVChargingStationsAPIService.swift
+++ EV Charging Station Finder/Data/DataSources/EVChargingStationsAPIService.swift
@@ -7,6 +7,23 @@
 
 import Foundation
 
-class EVChargingStationsAPIService {
-  // TODO: Not yet implemented
+class EVChargingStationsAPIService : EVChargingStationsRemoteDataSource {
+  private let session: URLSession
+  
+  init(session: URLSession = .shared) {
+    self.session = session
+  }
+
+  func fetchEVChargingStations(latitude: Double, longitude: Double, distance: Double) async throws -> [POIResponse] {
+    let baseUrl = OpenChargeMapAPI.baseURL
+    guard let openChargeMapKey: String = Bundle.main.infoDictionary?["OPEN_CHARGE_MAP_KEY"] as? String else {
+      throw NSError(domain: "EVChargingStationsAPIService", code: 1, userInfo: nil)
+    }
+    let stringUrl = "\(baseUrl)?output=json&lat=\(latitude)&lon=\(longitude)&distance=\(distance)&key=\(openChargeMapKey)"
+    let url = URL(string: OpenChargeMapAPI.baseURL)
+    
+    
+    return [POIResponse()]
+  }
 }
+
